6acc5ed99745855f89a8735c6b71fb4af1100d2d
OJIMA Ryoji <rojima1@gmail.com>
2018-02-03 10:58:22 +0900

    refactored.

--- src/functional2.mjs
+++ src/functional2.mjs
@@ -1,5 +1,5 @@
-import R                                 from 'ramda';
-import {adjustPath, iterate, maxIndexBy} from './utility';
+import R                        from 'ramda';
+import {adjustPath, maxIndexBy} from './utility';
 
 const lunchBoxes = [{'唐揚げ': {count: 10}, '玉子焼き': {count: 1}},
                     {'唐揚げ': {count:  8}},
@@ -9,7 +9,7 @@ const canEatIndex = (foodName, lunchBoxes) => maxIndexBy(R.path([foodName, 'coun
 const eatWithFinger = (foodName, lunchBoxes) => adjustPath([canEatIndex(foodName, lunchBoxes), foodName, 'count'], R.dec, lunchBoxes);
 
 export default function main() {
-  const eatKaraageWithFingerFiveTimes = iterate(R.curry(eatWithFinger)('唐揚げ'), 5);
+  const eatKaraageWithFingerFiveTimes = R.apply(R.pipe)(R.repeat(R.curry(eatWithFinger)('唐揚げ'), 5));
 
   return eatKaraageWithFingerFiveTimes(lunchBoxes);
 }
